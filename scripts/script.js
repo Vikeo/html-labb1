a7ca9ab5d7287292890ccda60002af8ff42a38c5
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2022-02-11 21:30:02 +0100

    add animation to hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,20 @@
-function toggleMenu() {
-	if (document.getElementById("menu-container").innerHTML == "") {
-		console.log("print menu");
-		printMenu();
-		//document.getElementById("menu").classList.add("menu");
-		//document.getElementById("menu-item").style.color = "white";
+function toggleMenu(menu) {
+	if (menu.dataset.state == "inactive") {
+		console.log("slide menu");
+		menuSlideIn(menu);
 	} else {
-		console.log("unprint menu");
-		unprintMenu();
+		console.log("unslide menu");
+		menuSlideOut(menu);
 	}
 }
 
-function printMenu() {
-	document.getElementById(
-		"menu-container"
-	).innerHTML = `<div id="menu" class="menu">
-                <a href="./index.html" class="menu-item">Hem</a>
-                <a href="./pages/courses.html" class="menu-item">Kurser</a>
-                <a href="#admin" class="menu-item">Admin</a>
-            </div>`;
+function menuSlideIn(menu) {
+	menu.classList.remove("menu-transition-out");
+	menu.classList.add("menu-transition-in");
+	menu.dataset.state = "active";
 }
-
-function unprintMenu() {
-	document.getElementById("menu-container").innerHTML = ``;
+function menuSlideOut(menu) {
+	menu.classList.add("menu-transition-out");
+	menu.classList.remove("menu-transition-in");
+	menu.dataset.state = "inactive";
 }
