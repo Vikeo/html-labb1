978650a4ca0fe6db6e9f4db98adb65e9464fe302
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2022-02-15 13:39:20 +0100

    start adding multiple courses

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,24 @@
+class Course {
+	constructor(id, title, description, descriptionDetailed, image) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.descriptionDetailed = descriptionDetailed;
+		this.image = image;
+	}
+}
+
+let cart = [];
+let courses = [];
+
+courses.push(
+	new Course("SS22HT", "Sälkurs", "hejsan", "hejsanhejsan", "./images/seal.jpg")
+);
+
+courses.push(
+	new Course(13, "Hjälpkurs", "hejsan", "hejsanhejsan", "./images/seal.jpg")
+);
+
 function toggleMenu(menu) {
 	if (menu.dataset.state == "inactive") {
 		console.log("slide menu");
@@ -18,3 +39,74 @@ function menuSlideOut(menu) {
 	menu.classList.remove("menu-transition-in");
 	menu.dataset.state = "inactive";
 }
+
+function addCourse(id, title, description, descriptionDetailed, image) {
+	//Ta in info, lägg till i en ny Course.
+	let newCourse = new Course(
+		id,
+		title,
+		description,
+		descriptionDetailed,
+		image
+	);
+	courses.push(newCourse);
+	console.log(courses);
+}
+
+function printCourseCards(coursesArray = []) {
+	coursesArray = courses;
+
+	//console.log(coursesArray);
+
+	coursesArray.forEach((course) => {
+		coursesHTML = document.getElementById("courses-list");
+
+		const div = document.createElement("div");
+		const img = document.createElement("img");
+		const h1 = document.createElement("h1");
+		const p = document.createElement("p");
+		const button = document.createElement("button");
+
+		div.classList.add("card");
+		img.classList.add("card-image");
+		h1.classList.add("card-title");
+		p.classList.add("card-text");
+		button.classList.add("card-add-button");
+
+		img.src = course.image;
+		img.alt = "card-image";
+		h1.innerText = course.title + ` (${course.id})`;
+		p.innerText = course.description;
+		button.innerText = "Lägg till";
+
+		//Lägger till funktionen för att lägga till boken i kundvagnen.
+		button.setAttribute("onclick", "addCourseToCart(" + course.id + ", cart);");
+
+		div.appendChild(img);
+		div.appendChild(h1);
+		div.appendChild(p);
+		div.appendChild(button);
+
+		coursesHTML.appendChild(div);
+	});
+}
+
+//Hittar kollar om ID:t på kurs-knappen man klickade på finns i en av objekten i courses arrayen, sen lägger till.
+function addCourseToCart(courseId, cart) {
+	console.log(courses);
+
+	const course = courses.find((el) => el.id === courseId);
+
+	cart.push(course);
+	console.log(`${course.title} tillagd`);
+}
+
+/* <div class="card">
+    <img class="card-image" src="./images/seal.jpg" alt="card-image">
+    <h1 class="card-title">Sälkurs (S22HT)</h1>
+    <p class="card-text">Lär dig om västkustens alla olika sälar i denna sälkurs! Här kommer du få rädda,
+        mata
+        och klappa massor av fina och tjocka sälar.
+    </p>
+    <button class="card-add-button ">Lägg till</button>
+</div> */
